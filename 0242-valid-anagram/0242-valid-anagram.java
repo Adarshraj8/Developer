866ae4554b4df055cb81cb518dce95944f57989b
Adarsh Raj <adarshrajgr2016@gmail.com>
2025-07-01 11:20:54 +0530

    Time: 6 ms (44.04%), Space: 43.2 MB (70.8%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,10 +3,17 @@ class Solution {
         
         if(s.length()!=t.length())return false;
           
-         char []a=s.toCharArray();
-         char []b= t.toCharArray();
-         Arrays.sort(a);
-         Arrays.sort(b);
-        return Arrays.equals(a,b);
+        int freq[] = new int[26];
+        
+        for(int i=0;i<s.length();i++){
+            
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
+        }
+        
+        for(int k :freq){
+            if(k!=0) return false;
+        }
+        return true;
     }
 }
