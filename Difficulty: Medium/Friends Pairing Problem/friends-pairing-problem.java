125f331ea3cebe0d3f5ec1658a1e4923760fcb7e
Adarsh Raj <adarshrajgr2016@gmail.com>
2025-10-25 21:27:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.java
+++ Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.java
@@ -2,13 +2,11 @@
 
 class Solution {
     public long countFriendsPairings(int n) {
-    HashMap<Integer,Long> dp = new HashMap<>();
-    if(n==1||n==0)
-    return 1;
-    if(dp.containsKey(n))return dp.get(n);
-    
-    long result = countFriendsPairings(n-1)+(long)(n-1)*countFriendsPairings(n-2);
-     dp.put(n,result);
-     return result;
-    }
+   
+   if(n<=1)
+   return 1;
+   
+   return countFriendsPairings(n-1)+(n-1)*countFriendsPairings(n-2);
+   
+}
 }
