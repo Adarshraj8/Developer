3f0129f7f90f15c0e15f871d595baade76adc655
Adarsh Raj <adarshrajgr2016@gmail.com>
2026-01-03 21:58:36 +0530

    Time: 0 ms (100%), Space: 43 MB (20.28%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -2,25 +2,23 @@ class Solution {
     public int rob(int[] num) {
         
       if(num.length==1) return num[0];
-
+      Integer memo1[]  = new Integer[num.length];
+      Integer memo2[]  = new Integer[num.length];
       return Math.max(
-        robRange(num,0,num.length-2),
-        robRange(num,1,num.length-1)
+        robRange(num,0,num.length-2,memo1),
+        robRange(num,1,num.length-1,memo2)
       );
 
     }
 
-    public int robRange(int num[],int start,int end){
-      
-    int dp[] = new int[end-start+1];
+    public int robRange(int num[],int start,int end,Integer memo[]){
+    
+    if(start>end) return 0;
+    if(memo[start]!=null) return memo[start];
+    
 
-     dp[0] = num[start];
-     if(end>start){
-        dp[1] = Math.max(num[start],num[start+1]);
-     }
-    for(int i=2;i<dp.length;i++){
-        dp[i]=Math.max(dp[i-1],num[start+i]+dp[i-2]);
-    }
-    return dp[dp.length-1];
+    memo[start]= Math.max(robRange(num,start+1,end, memo),
+                     num[start]+robRange(num,start+2,end,memo));
+    return memo[start];
     }
 }
