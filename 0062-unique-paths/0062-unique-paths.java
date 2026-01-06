cc8fd7083d7ce3347499338c71d35ca81513c827
Adarsh Raj <adarshrajgr2016@gmail.com>
2026-01-06 20:34:46 +0530

    Time: 0 ms (100%), Space: 42.6 MB (12.49%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,20 +1,14 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int ways[][] = new int[m][n];
+      int dp [][] = new int[m][n];
+      return dfs(m-1,n-1,dp);
+    }
+    public int dfs(int row, int col,int memo[][]){
+        if(row<0||col<0) return 0;
+        if(row==0&&col==0)  return 1;
+         if(memo[row][col]!=0) return memo[row][col];
 
-        for(int j=0;j<n;j++){
-            ways[0][j]=1;
-        }
-
-        for(int i=0;i<m;i++){
-            ways[i][0]=1;
-        }
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++)
-            {
-                ways[i][j] = ways[i-1][j]+ways[i][j-1]; 
-            }
-        }
-        return ways[m-1][n-1];
+        memo[row][col]= dfs(row-1,col,memo)+dfs(row,col-1,memo);
+        return memo[row][col];
     }
 }
