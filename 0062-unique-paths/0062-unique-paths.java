2a64725c4dc99b2de352af6ba81aabd8bc91db0c
Adarsh Raj <adarshrajgr2016@gmail.com>
2026-01-08 12:28:22 +0530

    Time: 0 ms (100%), Space: 42.1 MB (47.57%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -6,13 +6,15 @@ class Solution {
     public int dfs(int row, int col){
        // if(row<0||col<0) return 0;
         int dp[][]= new int[row][col];
-        //if(row==0&&col==0)  return 1;
+        if(row==0&&col==0)  return 1;
         // if(memo[row][col]!=0) return memo[row][col];
             for(int i=0;i<row;i++){
-            dp[i][0]=1;
+                dp[i][0]=1;
+               
             }
             for(int j=0;j<col;j++){
-                dp[0][j]=1;
+               dp[0][j]=1;
+                
             }
             for(int i=1;i<row;i++){
                 for(int j=1;j<col;j++){
