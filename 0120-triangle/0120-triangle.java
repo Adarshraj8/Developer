179863ea88bd5be7868035dd6d1e618f1109019d
Adarsh Raj <adarshrajgr2016@gmail.com>
2026-02-07 14:53:38 +0530

    Time: 3 ms (81.83%), Space: 45.5 MB (74.3%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,28 +1,17 @@
 class Solution {
-
-    //  Integer [][] memo;
     public int minimumTotal(List<List<Integer>> triangle) {
-   int n = triangle.size();
-    //memo = new Integer[n][n];
-    return solve(0,0,triangle);
+        
+        int n = triangle.size();
+        int dp[][]= new int[n][n];
+       for(int j=0;j<triangle.get(n-1).size();j++){
+        dp[n-1][j]=triangle.get(n-1).get(j);
+       }
 
-    }
-
-    public int solve(int row, int col,List<List<Integer>> triangle){
-
-       
-        int dp[] = new int[triangle.size()];
-          int n = triangle.size();
-         for(int j=0;j<triangle.get(n-1).size();j++){
-            dp[j]=triangle.get(n-1).get(j);
-         }
-        for(int i=n-2;i>=0;i--){
-            for(int j=0;j<=i;j++){
-         dp[j]= triangle.get(i).get(j)+Math.min(dp[j],dp[j+1]);
+      for(int i=n-2;i>=0;i--){
+        for(int j=0;j<=i;j++){
+            dp[i][j]=triangle.get(i).get(j)+Math.min(dp[i+1][j],dp[i+1][j+1]);
         }
-        }
-
-       
-       return dp[0];
+      }
+     return dp[0][0];
     }
 }
