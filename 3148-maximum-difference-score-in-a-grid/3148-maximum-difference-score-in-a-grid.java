d3ead1c9988ce1b44ce45db4bd466a6a28c373c3
Adarsh Raj <adarshrajgr2016@gmail.com>
2026-05-02 22:14:53 +0530

    Time: 10 ms (97.91%), Space: 105 MB (77.82%) - LeetHub

--- 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.java
+++ 3148-maximum-difference-score-in-a-grid/3148-maximum-difference-score-in-a-grid.java
@@ -2,37 +2,36 @@ import java.util.*;
 
 class Solution {
     public int maxScore(List<List<Integer>> grid) {
-        int m = grid.size();
-        int n = grid.get(0).size();
-
-        int[] dp = new int[n];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-
+       int m = grid.size();
+       int n = grid.get(0).size();
+       int dp[] = new int[n];
+       Arrays.fill(dp,Integer.MAX_VALUE);
         int maxScore = Integer.MIN_VALUE;
-
-        for (int i = 0; i < m; i++) {
+        for(int i=0;i<m;i++){
             int leftMin = Integer.MAX_VALUE;
-
-            for (int j = 0; j < n; j++) {
+            for(int j=0;j<n;j++){
                 int current = grid.get(i).get(j);
 
-                int minPrev = Integer.MAX_VALUE;
+                int minPrev= Integer.MAX_VALUE;
 
-                if (i > 0) minPrev = Math.min(minPrev, dp[j]);     // from top
-                if (j > 0) minPrev = Math.min(minPrev, leftMin);  // from left
-
-                // ensure at least one move
-                if (i > 0 || j > 0) {
-                    maxScore = Math.max(maxScore, current - minPrev);
+                if(i>0){
+                    minPrev = Math.min(minPrev,dp[j]);//top
+                }
+                if(j>0){
+                    minPrev= Math.min(minPrev,leftMin);//left
                 }
 
-                int currMin = Math.min(current, minPrev);
+                if(i>0||j>0){
+                    maxScore= Math.max(maxScore,current-minPrev);
+                }
 
-                dp[j] = currMin;
-                leftMin = currMin;
-            }
+
+                int currMin= Math.min(minPrev,current);
+                 
+                 dp[j] = currMin;// for next row
+                 leftMin=currMin;// for next column
+              }
         }
-
         return maxScore;
     }
 }
