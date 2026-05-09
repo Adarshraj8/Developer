ff0f8a2133a0f786e116b951b9be91152b791834
Adarsh Raj <adarshrajgr2016@gmail.com>
2026-05-09 16:45:28 +0530

    Time: 1661 ms (5.03%), Space: 69.4 MB (52.38%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -9,7 +9,7 @@ class Solution {
 
         n = startTime.length;
 
-        // Step 1: Combine into jobs array
+        // Step 1: Combine in  to jobs array
         jobs = new int[n][3];
         for (int i = 0; i < n; i++) {
             jobs[i][0] = startTime[i]; // start
@@ -19,31 +19,33 @@ class Solution {
 
         // Step 2: Sort by start time
         Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
-         dp = new int[n];
+         dp = new int[n+1];
         // Step 3: Call recursion
         return solve(0);
     }
 
-    // 🔥 Brute force recursion
+    
     private int solve(int i) {
 
         // Base case
-        if (i == n) return 0;
-      if(dp[i]!=0)return dp[i];
+       // if (i == n) return 0;
+        //if(dp[i]!=0)return dp[i];
+        for(int j=n-1;j>=0;j--){
         // OPTION 1: Skip current job
-        int skip = solve(i + 1);
+        int skip = dp[j+ 1];
            
         // OPTION 2: Take current job
-        int next = i + 1;
+        int next = j + 1;
 
         // Find next non-overlapping job
-        while (next < n && jobs[next][0] < jobs[i][1]) {
+        while (next < n && jobs[next][0] < jobs[j][1]) {
             next++;
         }
 
-        int take = jobs[i][2] + solve(next);
-        dp[i] = Math.max(skip, take);
+        int take = jobs[j][2] + dp[next];
+        dp[j] = Math.max(skip, take);
+        }
         // Return maximum
-        return dp[i];
+        return dp[0];
     }
 }
