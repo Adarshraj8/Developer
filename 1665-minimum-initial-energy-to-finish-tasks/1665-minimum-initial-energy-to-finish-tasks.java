f629b4faaf463e20e3e7b5a8172c5ea9eff94a3b
Adarsh Raj <adarshrajgr2016@gmail.com>
2026-05-18 20:32:38 +0530

    Time: 22 ms (99.72%), Space: 114.2 MB (14.29%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
@@ -2,41 +2,14 @@ class Solution {
     public int minimumEffort(int[][] tasks) {
      
        Arrays.sort(tasks,(a,b)->(b[1]-b[0])-(a[1]-a[0]));
-
-       	   int hi=0;
-		   int low=1;
-		   
-		   for(int task[]:tasks)
-		    hi +=task[1];
-			
-			int answer = hi;
-			
-			while(low<=hi){
-			int mid = low+(hi-low)/2;
-			if(canReach(tasks,mid)){
-			  answer=mid;
-			  hi=mid-1;
-			  }else{
-			   low=mid+1;
-			   }
-			   }
-			 return answer;  
+            
+            int answer=0;
+            int required=0;
+       	   for(int i=tasks.length-1;i>=0;i--){
+            required = Math.max(tasks[i][1],required+tasks[i][0]);
+           }
+          
+           return required;
     }   
 	
-	
-	 private boolean canReach(int tasks[][],int initialEnergy){
-	 
-	 int energy = initialEnergy;
-	 for(int task[]:tasks){
-      int actual = task[0];
-      int minimum=task[1];
-      
-	  if(energy<minimum)return false;
-	  
-	  energy = energy - actual;
-	  
-	 
-	  }
-       	  return true;
-}
 }
