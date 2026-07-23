304237e281898f884eeebd4c2988a6c144270ccc
Adarsh Raj <adarshrajgr2016@gmail.com>
2026-07-23 22:41:16 +0530

    Time: 1 ms (86.31%), Space: 44.2 MB (92.66%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -2,29 +2,26 @@ class Solution {
     public List<String> generateParenthesis(int n) {
         
         List<String> result = new ArrayList<>();
-        if(n<=0)return result;
-          backtrack(n,new StringBuilder(),0,0,result);
-          return result;
+        backTrack(result,new StringBuilder(),0,0,n);
+        return result;
     }
 
-    public void backtrack(int n,StringBuilder sb,int open,int close,List<String> result){
-        if(sb.length()==2*n){
-            result.add(sb.toString());
-            return;
-        }
+    private void backTrack(List<String> result,StringBuilder sb,int open,int close,int n){
+     
+       if(sb.length()==2*n){
+          result.add(sb.toString());
+          return;
+       }
 
-        if(open<n){
-           sb.append('(');
-           backtrack(n,sb,open+1,close,result);
-           sb.deleteCharAt(sb.length()-1);
-
-        }
-
-        if(close<open){
-            sb.append(')');
-            backtrack(n,sb,open,close+1,result);
-              sb.deleteCharAt(sb.length()-1);
-        }
-        
+       if(open<n){
+        sb.append('(');
+         backTrack(result,sb,open+1,close,n);
+         sb.deleteCharAt(sb.length()-1);
+       }if(close<open){
+         sb.append(')');
+         backTrack(result,sb,open,close+1,n);
+         sb.deleteCharAt(sb.length()-1);
+       }
+       
     }
 }
