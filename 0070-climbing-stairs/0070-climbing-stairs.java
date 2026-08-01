34a730dccaca244b3e62bb8af5bc348f6d914cb8
Adarsh Raj <adarshrajgr2016@gmail.com>
2026-08-01 19:30:35 +0530

    Time: 0 ms (100%), Space: 42 MB (73.43%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
-        int total_way=0;
-        for(int k=0;k<=n/2;k++){
-            int ones = n-2*k;
-            int total_moves = k+ones;
-            long way_for_this_k =C(total_moves,k);
-            total_way += way_for_this_k;
-        }
-        return total_way;
+        
+        int memo[] = new int[n+1];
+        Arrays.fill(memo,-1);
+        return findWays(memo,n);
+
     }
 
-    private long C(int a, int b){
-        long result=1;
-        for(int i=1;i<=b;i++){
-            result= result*(a-b+i)/i;
-        }
-        return result;
-    }
+  private int findWays(int memo[],int n){
+    if(n<=2)
+    return n;
+
+    if(memo[n]!=-1)
+    return memo[n];
+
+    memo[n]= findWays(memo,n-1)+findWays(memo,n-2);
+    return memo[n];
+  }
 }
