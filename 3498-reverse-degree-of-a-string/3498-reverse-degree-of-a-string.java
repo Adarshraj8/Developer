916081e8740f36da1e12eb4df8a3acac303d0845
Adarsh Raj <adarshrajgr2016@gmail.com>
2026-09-21 22:52:53 +0530

    Time: 1 ms (100%), Space: 44 MB (50.3%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -5,6 +5,7 @@ class Solution {
         int sum=0;
         for(int i=0;i<n;i++){
         int asc= 26- (s.charAt(i)-'a');
+    // Change idx to be the 1-based index of the character in the string
         int idx = i+1;
          
          sum+=asc*idx;
